fe509c8adcb359ca7a4cacd6e50ef35d10500dc5
anilnunnagopula <btech2k26@gmail.com>
2025-06-26 02:00:42 +0530

    o

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,11 +23,8 @@ const Login = () => {
 
   try {
     // const response = await fetch("http://localhost:5000/api/auth/login", {
-      // const response = await fetch(
-      //   `${process.env.REACT_APP_API_URL}/api/auth/login`,
-      //temporary
       const response = await fetch(
-        "https://connectingconstructions1-backend.onrender.com/api/auth/login",
+        `${process.env.REACT_APP_API_URL}/api/auth/login`,
         {
           method: "POST",
           headers: {
